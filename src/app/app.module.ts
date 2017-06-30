b86d4450e42eee2c51cd6d7e9059df6a5da93a3b
Nate Rauh <nrauh@users.noreply.github.com>
2017-06-29 19:45:28 -0500

    Added router and generated messages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SwiperComponent } from './swiper/swiper.component';
 import { CardComponent } from './swiper/card/card.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MessagesComponent } from './messages/messages.component';
+
+let appRoutes: Routes = [
+  { path: '', component: SwiperComponent },
+  { path: 'messages', component: MessagesComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SwiperComponent,
     CardComponent,
-    NavbarComponent
+    NavbarComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
